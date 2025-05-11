131af1298a9ca6f8132da643f26f10ef630d7736
dev-seokho <loyu78@gmail.com>
2025-05-11 15:37:56 +0900

    ADD: 반환 객체 공통화를 위한 RestControllerAdvice 추가

--- src/main/kotlin/com/sync/sprout/support/annotation/NoWrapViewEntity.kt
+++ src/main/kotlin/com/sync/sprout/support/annotation/NoWrapViewEntity.kt
@@ -1,3 +1,5 @@
 package com.sync.sprout.support.annotation
 
+@Target(AnnotationTarget.FUNCTION)
+@Retention(AnnotationRetention.RUNTIME)
 annotation class NoWrapViewEntity()
--- src/main/kotlin/com/sync/sprout/support/web/ApiResponseAdvice.kt
+++ src/main/kotlin/com/sync/sprout/support/web/ApiResponseAdvice.kt
@@ -1,4 +1,36 @@
 package com.sync.sprout.support.web
 
-class ApiResponseAdvice {
+import com.sync.sprout.support.annotation.NoWrapViewEntity
+import org.springframework.core.MethodParameter
+import org.springframework.http.MediaType
+import org.springframework.http.converter.HttpMessageConverter
+import org.springframework.http.server.ServerHttpRequest
+import org.springframework.http.server.ServerHttpResponse
+import org.springframework.web.bind.annotation.RestControllerAdvice
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice
+
+@RestControllerAdvice
+class ApiResponseAdvice : ResponseBodyAdvice<Any> {
+
+    override fun supports(
+        returnType: MethodParameter,
+        converterType: Class<out HttpMessageConverter<*>?>
+    ): Boolean {
+        return !(returnType.parameterType == String::class.java ||
+                returnType.method?.name == "openapiJson" ||
+                returnType.getMethodAnnotation(NoWrapViewEntity::class.java) != null)
+    }
+
+    override fun beforeBodyWrite(
+        body: Any?,
+        returnType: MethodParameter,
+        selectedContentType: MediaType,
+        selectedConverterType: Class<out HttpMessageConverter<*>?>,
+        request: ServerHttpRequest,
+        response: ServerHttpResponse
+    ): Any? {
+        return body as? ViewEntity ?: ViewEntity.Success(
+            data = body
+        )
+    }
 }
